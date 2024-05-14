f3de934bdd4880c56452908cd5702edbf6bf4391
QPM <admin@QuantumPinnacleMatrix.tech>
2024-05-14 18:13:05 +0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Aora!</Text>
+      <StatusBar />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
